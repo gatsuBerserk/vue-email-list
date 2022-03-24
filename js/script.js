bd8760a1ecc6209f21b09fbdba46d4f22101f46a
gatsuBerserk <berserkdev@outlook.it>
2022-03-24 16:15:44 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const app = new Vue(
+    {
+        el:"#app",
+        data:{}, 
+        methods:{},
+}) 
+
+// Prova per capire il funzionamento
+// axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+//     .then(function(response){ 
+//         const result = response;
+//         console.log(result.data.response)
+//     }); 
+
